bff9599eb504af4b1254c710665cc429bbc03200
Alwurts <jota780@gmail.com>
2025-03-02 20:04:12 +0100

    Add shadcn with tailwind v4

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
+import path from "node:path";
 
 export default defineConfig({
   plugins: [cloudflare(), react(), tailwindcss()],
+  resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 });
